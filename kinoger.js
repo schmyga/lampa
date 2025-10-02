b8637c38d93d58feb3a3b7d3288f713724147527
schmyga <93492278+schmyga@users.noreply.github.com>
2025-10-02 13:24:27 +0200

    Update kinoger.js

--- kinoger.js
+++ kinoger.js
@@ -3,23 +3,22 @@
     console.log('Kinoger test started at ' + new Date().toLocaleTimeString());
 
     try {
-        function component() {
+        function component(object) {
             this.initialize = function() {
                 console.log('Kinoger component initialized at ' + new Date().toLocaleTimeString());
                 var scroll = new Lampa.Scroll({ mask: true, over: true });
-                scroll.body().append('<div>Kinoger Test Menu</div>');
+                scroll.body().append('<div>Kinoger Test Menu (v6)</div>');
                 Lampa.Controller.enable('content');
             };
         }
 
         function startPlugin() {
             console.log('Starting Kinoger plugin at ' + new Date().toLocaleTimeString());
-            window.kinoger_plugin = true;
             if (typeof Lampa !== 'undefined' && Lampa.Component && Lampa.Menu) {
-                Lampa.Component.add('kinoger_test_v5', component);
-                Lampa.Menu.add('kinoger_test_menu_v5', {
-                    title: 'Kinoger Test Menu v5',
-                    url: 'plugin/kinoger_test_v5',
+                Lampa.Component.add('kinoger_test_v6', component);
+                Lampa.Menu.add('kinoger_test_menu_v6', {
+                    title: 'Kinoger Test Menu v6',
+                    url: 'plugin/kinoger_test_v6',
                     type: 'catalog'
                 });
                 console.log('Kinoger plugin registered at ' + new Date().toLocaleTimeString());
@@ -28,7 +27,10 @@
             }
         }
 
-        if (!window.kinoger_plugin) startPlugin();
+        if (!window.kinoger_plugin) {
+            window.kinoger_plugin = true;
+            startPlugin();
+        }
     } catch (e) {
         console.error('Kinoger error at ' + new Date().toLocaleTimeString() + ':', e);
     }
